39993ed352fea8e956c5c83bfd3bec70760ddac9
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-09 21:31:56 +0530

    Future thoughts

--- Adobe.java
+++ Adobe.java
@@ -4,6 +4,6 @@ public class Adobe {
     public static void main(String[] args) {
 
         System.out.println("SDET at Adobe");
-        
+        System.out.println("In next 5 years I'll be RICH");
     }
 }
